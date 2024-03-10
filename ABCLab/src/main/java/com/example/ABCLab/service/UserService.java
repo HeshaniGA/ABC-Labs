b3f0d4b6c7f90ea77f9108fbcffce9ecb1c01c52
HeshaniGA <heshanigunasekara56@gmail.com>
2024-03-10 20:44:40 +0530

    feat(basic auth)

---
+++ ABCLab/src/main/java/com/example/ABCLab/service/UserService.java
@@ -0,0 +1,11 @@
+package com.example.ABCLab.service;
+
+import com.example.ABCLab.dto.UserDto;
+import com.example.ABCLab.model.User;
+
+public interface UserService {
+	
+	User save (UserDto userDto);
+	
+
+}
---
+++ ABCLab/src/main/java/com/example/ABCLab/service/UserServiceImpl.java
@@ -0,0 +1,26 @@
+package com.example.ABCLab.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import com.example.ABCLab.dto.UserDto;
+import com.example.ABCLab.model.User;
+import com.example.ABCLab.repositories.UserRepository;
+
+@Service
+public class UserServiceImpl implements UserService {
+	
+	@Autowired
+	private PasswordEncoder passwordEncoder;
+	
+	@Autowired
+	private UserRepository userRepository;
+
+	@Override
+	public User save(UserDto userDto) {
+		User user = new User(userDto.getEmail(), passwordEncoder.encode(userDto.getPassword()) , userDto.getRole(), userDto.getFullname());
+		return userRepository.save(user);
+	}
+
+}
